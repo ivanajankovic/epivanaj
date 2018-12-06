44e369e6c8b38721722685e975cf44dd64d3e1ee
Ivana <ivana.smilcic@gmail.com>
2018-12-06 23:44:53 +0100

    Add files via upload

---
+++ about_us.php
@@ -0,0 +1,82 @@
+
+<?php include("include/header.php");?>
+
+
+<div class="container">
+	<div class="row text-center">
+		<h2 class="text-uppercase">About us</h2>
+		<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. 
+		Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.A small river named Duden flows by their place and supplies it with the necessary regelialia. 
+        It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+	</div>
+</div>
+<hr>
+<div class="container">
+	<div class="row">
+    	<div class="col-md-6 col-sm-6 col-xs-12">
+    		<div class="embed-responsive embed-responsive-16by9">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/0ji-OF3KWW0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            </div>
+    	</div>
+    	<div class="col-md-6 col-sm-6 col-xs-12">
+    		<h2 class="text-uppercase">About us <sup class="badge">with video</sup></h2>
+    		<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. 
+    		Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.A small river named Duden flows by their place and supplies it with the necessary regelialia. 
+            It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+    	</div>
+	</div>
+</div>
+<hr>
+<div class="container">
+	<div class="row">
+    	<div class="col-md-6 col-sm-6 col-xs-12">
+    		<h2 class="text-uppercase">About us <sup class="badge">with image</sup></h2>
+    		<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. 
+    		Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.A small river named Duden flows by their place and supplies it with the necessary regelialia. 
+            It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+    	</div>
+    	<div class="col-md-6 col-sm-6 col-xs-12">
+    	    <img src="http://placehold.it/555x312/652fff/ffffff/?text=ABOUT%20US" alt="" class="img-responsive">
+    	</div>
+	</div>
+</div>
+<br><br>
+<div class="bg-primary">
+    <div class="container">
+    	<div class="row">
+        	<br>
+    		<h1 class="text-center text-uppercase">About us</h1>
+        	<div class="text-center">
+        		<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. 
+        		Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.A small river named Duden flows by their place and supplies it with the necessary regelialia. 
+                It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+        	</div>
+        	<br>
+        	<div class="">
+        	    <img src="http://placehold.it/555x312/ff9c00/ffffff/?text=ABOUT%20US" alt="" class="img-responsive center-block">
+        	</div>
+        	<br>
+        	<br>
+    	</div>
+    </div>
+</div>
+
+				
+<?php include("include/footer.php");?>
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+				
+				
